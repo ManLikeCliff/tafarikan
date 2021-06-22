08a71561b5fc0aef6833b0c8da05ab371cd4d579
Chukwuebuka Cliff <klifiam@gmail.com>
2021-06-22 16:42:04 +0100

    Added Home Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,12 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
 
