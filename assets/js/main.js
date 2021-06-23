5c18d6c2747fbc876bd6d02aa0c7dc245731d413
Chukwuebuka Cliff <klifiam@gmail.com>
2021-06-23 13:52:23 +0100

    Added Discover Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,17 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
-
+var swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+      rotate: 0,
+    },
+  });
 
 /*==================== VIDEO ====================*/
 
