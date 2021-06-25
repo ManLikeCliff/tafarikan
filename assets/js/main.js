c3ccffa386dfdbaf06557e93d73f095a0eb9783a
Chukwuebuka Cliff <klifiam@gmail.com>
2021-06-25 02:58:06 +0100

    Added Footer Section and Scrollup indicator

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,12 @@ function finalVideo(){
 videoFile.addEventListener('ended', finalVideo)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
